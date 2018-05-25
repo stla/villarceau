d11348904fc67239025e56be5307db050527b6f9
stla <laurent_step@yahoo.fr>
2018-05-25 21:35:01 +0200

    Utils folder

---
+++ src/Utils/Hopf.hs
@@ -0,0 +1,28 @@
+module Utils.Hopf
+  where
+
+hopfinverse :: (Num a, Floating a) => a -> a -> a -> a -> (a,a,a,a)
+hopfinverse q0 q1 q2 t =
+  let h = (1 / sqrt (2 * (1+q2))) in
+  (h* (cos t * q0 + sin t * q1),
+   h * sin t * (1+q2),
+   h * cos t * (1+q2),
+   h * (sin t * q0 - cos t * q1))
+
+-- hopfinverse' :: [GLfloat] -> GLfloat -> Vertex3 GLfloat
+-- hopfinverse' q t = toVx3 (hopfinverse q t)
+--    where
+--      toVx3 x = Vertex3 (x!!0) (x!!1) (x!!2)
+
+stereoProj :: (Num a, Fractional a) => (a,a,a,a) -> (a,a,a)
+stereoProj (x0,x1,x2,x3) =
+  let h = 1/(1-x3) in
+  (h * x0, h * x1, h * x2)
+
+stereoCircHinv :: (Num a, Floating a) => a -> a -> a -> (a, a, a)
+stereoCircHinv theta phi xi =
+  stereoProj (hopfinverse q0 q1 q2 xi)
+  where
+    q0 = cos theta * cos phi
+    q1 = sin theta * cos phi
+    q2 = sin phi
---
+++ src/Utils/TestTransformationMatrix.hs
@@ -0,0 +1,134 @@
+module Utils.TestTransformationMatrix
+  where
+import           Data.IORef
+import           Data.Tuple.Extra             (fst3, snd3, thd3)
+import           Graphics.Rendering.OpenGL.GL
+import           Graphics.UI.GLUT
+import           Linear                       (V3 (..))
+import           Utils.TransformationMatrix
+
+type Point = (GLfloat,GLfloat,GLfloat)
+
+pointToV3 :: Point -> V3 GLfloat
+pointToV3 (x,y,z) = V3 x y z
+
+myTriplet :: (Point, Point, Point)
+myTriplet = ((-1,5,1),(2,1,8),(-5,0,3)) -- issue if in plane z=0 (or plane z=d ?)
+
+data Context = Context
+    {
+      contextRot1 :: IORef GLfloat
+    , contextRot2 :: IORef GLfloat
+    , contextRot3 :: IORef GLfloat
+    , contextZoom :: IORef Double
+    }
+
+white,black,red,green,blue :: Color4 GLfloat
+white      = Color4    1    1    1    1
+black      = Color4    0    0    0    1
+red        = Color4    1    0    0    1
+green      = Color4    0    1    0    0.5
+blue       = Color4    0    0    1    0.5
+
+tmatAndRadius :: ([GLfloat], GLdouble)
+tmatAndRadius = transformationMatrix
+                (pointToV3 $ fst3 myTriplet)
+                (pointToV3 $ snd3 myTriplet)
+                (pointToV3 $ thd3 myTriplet)
+
+transfo :: IO ()
+transfo = do
+  m <- newMatrix RowMajor (fst tmatAndRadius) :: IO (GLmatrix GLfloat)
+  multMatrix m
+
+radius :: GLdouble
+radius = snd tmatAndRadius
+
+display :: Context -> DisplayCallback
+display context = do
+  clear [ColorBuffer, DepthBuffer]
+  r1 <- get (contextRot1 context)
+  r2 <- get (contextRot2 context)
+  r3 <- get (contextRot3 context)
+  zoom <- get (contextZoom context)
+  (_, size) <- get viewport
+  loadIdentity
+  resize zoom size
+  rotate r1 $ Vector3 1 0 0
+  rotate r2 $ Vector3 0 1 0
+  rotate r3 $ Vector3 0 0 1
+  preservingMatrix $ do
+    transfo
+    materialDiffuse FrontAndBack $= blue
+    renderObject Solid $ Torus 0.1 radius 30 30
+  renderPrimitive Triangles $ do
+    materialDiffuse FrontAndBack $= red
+    mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) [fst3 myTriplet, snd3 myTriplet, thd3 myTriplet]
+  swapBuffers
+
+resize :: Double -> Size -> IO ()
+resize zoom s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+  matrixMode $= Projection
+  loadIdentity
+  perspective 45.0 (w'/h') 1.0 100.0
+  lookAt (Vertex3 0 0 (-20+zoom)) (Vertex3 0 0 0) (Vector3 0 1 0)
+  matrixMode $= Modelview 0
+  where
+    w' = realToFrac w
+    h' = realToFrac h
+
+keyboard :: IORef GLfloat -> IORef GLfloat -> IORef GLfloat -- rotations
+         -> IORef GLdouble -- zoom
+         -> KeyboardCallback
+keyboard rot1 rot2 rot3 zoom c _ = do
+  case c of
+    'e' -> rot1 $~! subtract 2
+    'r' -> rot1 $~! (+2)
+    't' -> rot2 $~! subtract 2
+    'y' -> rot2 $~! (+2)
+    'u' -> rot3 $~! subtract 2
+    'i' -> rot3 $~! (+2)
+    'm' -> zoom $~! (+1)
+    'l' -> zoom $~! subtract 1
+    'q' -> leaveMainLoop
+    _   -> return ()
+  postRedisplay Nothing
+
+main :: IO ()
+main = do
+  _ <- getArgsAndInitialize
+  _ <- createWindow "Torus passing by three points"
+  windowSize $= Size 500 500
+  initialDisplayMode $= [RGBAMode, DoubleBuffered, WithDepthBuffer]
+  clearColor $= white
+  materialAmbient FrontAndBack $= black
+  lighting $= Enabled
+  lightModelTwoSide $= Enabled
+  light (Light 0) $= Enabled
+  position (Light 0) $= Vertex4 0 0 (-100) 1
+  ambient (Light 0) $= black
+  diffuse (Light 0) $= white
+  specular (Light 0) $= black
+  depthFunc $= Just Less
+  shadeModel $= Smooth
+  blend $= Enabled    -- allow transparency
+  blendFunc $= (SrcAlpha, OneMinusSrcAlpha)
+  rot1 <- newIORef 0.0
+  rot2 <- newIORef 0.0
+  rot3 <- newIORef 0.0
+  zoom <- newIORef 0.0
+  displayCallback $= display Context { contextRot1 = rot1,
+                                       contextRot2 = rot2,
+                                       contextRot3 = rot3,
+                                       contextZoom = zoom }
+  reshapeCallback $= Just (resize 0)
+  keyboardCallback $= Just (keyboard rot1 rot2 rot3 zoom)
+  idleCallback $= Nothing
+  putStrLn "*** Torus passing by three points ***\n\
+        \    To quit, press q.\n\
+        \    Scene rotation:\n\
+        \        e, r, t, y, u, i\n\
+        \    Zoom: l, m\n\
+        \"
+  mainLoop
